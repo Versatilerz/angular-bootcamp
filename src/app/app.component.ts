88503642cd96a6d517c206081bb4ddbd4eed7aa2
Nils van Gestel <nvangestel@yellowtail.nl>
2024-11-09 15:40:00 +0100

    first component users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'ClientApp';
-  hellowWorld: string = 'Hell World';
+  hellowWorld: string = 'Hello World';
 }
