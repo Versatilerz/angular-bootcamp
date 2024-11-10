5a95869930336e738f1b00d931802db35ad3b299
Nils van Gestel <nvangestel@yellowtail.nl>
2024-11-10 11:19:20 +0100

    first service

--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
 
 @Component({
   selector: 'app-user-single',
@@ -6,7 +7,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user-single.component.css',
 })
 export class UserSingleComponent {
-  @Input() user: string = '';
+  // @Input() user: string = '';
   @Input() index: number = -1;
   @Output() deleteUser: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor(public userService: UserService) {}
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user-service.service';
 
 @Component({
   selector: 'app-users',
@@ -6,36 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
-  userList = [
-    'Tucker Anselm',
-    'Elmira Keddy',
-    'Eveline Grandisson',
-    'Berry Wildes',
-    'Quintus Hastings',
-    'Harp Antonignetti',
-    'Vite Playfair',
-    'Noelle Dowears',
-    'Delcine Lubbock',
-    'Auberta Skerrett',
-    'Constantin Cosgry',
-    'Loleta Grenfell',
-    'Nadeen Matchett',
-    'Elli Galliver',
-    'Gayla Hawtin',
-    'Liam Antwis',
-    'Merilyn Baumford',
-    'Lilas Colquyte',
-    'Roi Kinworthy',
-    'Patin Flecknoe',
-    'Etienne Vedeneev',
-    'Diane Evesque',
-    'Ashlee Amoore',
-    'Julissa Bandey',
-    'Merridie McPartling',
-    'Nanete Kitlee',
-  ];
+  // userList = [
+  //   'Tucker Anselm',
+  //   'Elmira Keddy',
+  //   'Eveline Grandisson',
+  //   'Berry Wildes',
+  //   'Quintus Hastings',
+  //   'Harp Antonignetti',
+  //   'Vite Playfair',
+  //   'Noelle Dowears',
+  //   'Delcine Lubbock',
+  //   'Auberta Skerrett',
+  //   'Constantin Cosgry',
+  //   'Loleta Grenfell',
+  //   'Nadeen Matchett',
+  //   'Elli Galliver',
+  //   'Gayla Hawtin',
+  //   'Liam Antwis',
+  //   'Merilyn Baumford',
+  //   'Lilas Colquyte',
+  //   'Roi Kinworthy',
+  //   'Patin Flecknoe',
+  //   'Etienne Vedeneev',
+  //   'Diane Evesque',
+  //   'Ashlee Amoore',
+  //   'Julissa Bandey',
+  //   'Merridie McPartling',
+  //   'Nanete Kitlee',
+  // ];
+
+  constructor(public userService: UserService) {}
 
   deleteUser = (index: number) => {
-    this.userList.splice(index, 1);
+    this.userService.userList.splice(index, 1);
   };
 }
