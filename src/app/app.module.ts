7829b52f320e5521f7d0389c3b9e3c2c1625ff21
Nils van Gestel <nvangestel@yellowtail.nl>
2024-11-10 10:23:48 +0100

    parent to child input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { FormsModule } from '@angular/forms';
+import { UserSingleComponent } from './users/user-single/user-single.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersComponent],
+  declarations: [AppComponent, UsersComponent, UserSingleComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
